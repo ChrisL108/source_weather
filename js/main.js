d45edd1afa1a6e2f167b08e9cf777ee502287252
Christopher Lacaille <Christopher Lacaille>
2016-05-06 03:06:00 -0500

    now builds proper API url with longitude and latitude

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(function() {
 
-});
+	if ("geolocation" in navigator) {
+		console.log("supported");
+		navigator.geolocation.getCurrentPosition(success, fail);
+	} else {
+		alert("Your browser doesn't support Geolocation :/");
+	}
+
+	function success(position) {
+		// Get User Coordinates
+		var lon = position.coords.longitude;
+		var lat = position.coords.latitude;
+		// Build URL using user long/lat and my API key
+		var id = "&APPID=73534e4671149a5202f94d9eaf058256",
+			units = "&units=imperial",
+			base =  "http://api.openweathermap.org/data/2.5/weather?lat="+ lat + "&lon=" + lon;
+		var fullUrl = base + id + units;
+		console.log(fullUrl);
+	}
+	function fail() {
+		alert("Failed to get your location. Is your browser blocking it?");
+	}
+
+ }); //doc-ready
